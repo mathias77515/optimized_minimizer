1eb2ea2fd3e31ba358bdba603b1fe11ef8d0d62e
mregnier <mathias.p.regnier@gmail.com>
2023-05-24 11:59:27 +0200

    1st commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='optimized_minimizer',
+    url='',
+    author='Mathias Regnier',
+    author_email='mathias.p.regnier@gmail.com',
+    # Needed to actually package something
+    packages=['optimized_minimizer'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='',
+    description='Optimized minimizer for MPI python.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
